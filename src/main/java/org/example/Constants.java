6925fb7f2e984de6bb575926d7372acd03c79487
David Even Haim <davidevenhai@gmail.com>
2023-06-07 10:28:47 +0300

    done

--- src/main/java/org/example/Constants.java
+++ src/main/java/org/example/Constants.java
@@ -4,7 +4,6 @@ public class Constants {
 
     public static final int WIDTH = 800;
     public static final int HEIGHT = 600;
-    public static final int xLocation = 545;
     public static final String MESSAGE_STATUS = "Message Status: ";
     public static final String AREA_CODE = "9725";
     public static final String ISRAELI_PHONE = "05";
